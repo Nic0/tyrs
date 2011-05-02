7b8c3452bac9e4e092750b044f7f6168d847e9ce
Nicolas Paris <nicolas.caen@gmail.com>
2011-05-02 17:22:34 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+from distutils.core import setup
+
+setup(name='tyrs',
+      version='0.1',
+      description='Twitter client based on curses',
+      long_description=
+      '''TODO''',
+
+      author='Nicolas Paris',
+      author_email='nicolas.caen@gmail.com',
+      license='GPLv3',
+      requires=['python-twitter'],
+      packages=['tyrs'],
+    )
