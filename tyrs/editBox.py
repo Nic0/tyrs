959d58cfd1980cd7e145ff533a9ce5e5fb53c922
Nicolas Paris <nicolas.caen@gmail.com>
2011-05-07 20:33:41 +0200

    better looking input

--- tyrs/editBox.py
+++ tyrs/editBox.py
@@ -1,5 +1,3 @@
-# -*- coding:utf-8 -*-
-
 import sys
 import curses
 import curses.textpad
@@ -12,30 +10,31 @@ class EditBox:
 
         self.win = self.initWin(screen)
         self.startEdit()
-        print 'tweet: %s' % self.tweet
 
     def startEdit (self):
         tweet = ''
+        maxyx = self.win.getmaxyx()
 
         while True:
             ch = self.win.getch()
 
-            if ch == 10:        # Corresponding to ENTER
+            if ch == 10:          # ENTER: send the tweet
                 self.confirm = True
                 break
-            elif ch == 27:        # Corresponding to ESC
+            elif ch == 27:        # ESC: abord
                 break
 
             else:
                 cur_yx = self.win.getyx()
 
                 # for new lines, we don't want to start right in the border
-                if cur_yx[1] == 0:
-                    self.win.move(cur_yx[0], 3)
+                # we move the cursor, and get the correct value back to cur_yx
+                if cur_yx[1] == maxyx[1] - 2:
+                    self.win.move(cur_yx[0] +1, 2)
+                    cur_yx = self.win.getyx()
 
                 tweet += chr(ch)
                 self.win.addstr(cur_yx[0], cur_yx[1], chr(ch))
-
 
         self.tweet = tweet
 
@@ -48,15 +47,15 @@ class EditBox:
         maxyx = screen.getmaxyx()
 
         # Set width
-        if maxyx[0] > 80:
+        if maxyx[1] > 80:
             width = 80
         else:
-            width = maxyx[0] - 4
+            width = maxyx[1] - 4 # 4: leave 2pix on every side at least
 
         # Set height
         height = int(200 / width) + 4
 
-        # Start of EditWin
+        # Start of EditWin, display in the middle of the main screen
         start_y = maxyx[0]/2 - int(height/2)
         start_x = maxyx[1]/2 - int(width/2)
         self.sizeyx = (height, width)
@@ -68,7 +67,7 @@ class EditBox:
 
         win.border(0)
         win.addstr(0, 3, ' What\'s up ? ', curses.color_pair(3))
-        win.move(2, 3)
+        win.move(2, 2)
         return win
 
     def getTweet (self):
