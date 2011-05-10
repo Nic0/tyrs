f637ba0b1c9842a2de243c3d10a1bc09508aa528
Nicolas Paris <nicolas.caen@gmail.com>
2011-05-11 00:57:47 +0200

    authentification bug should fixed

--- tyrs/tweets.py
+++ tyrs/tweets.py
@@ -4,10 +4,10 @@ import urllib2
 class Tweets:
 
     def authentification(self, conf):
-        self.api = twitter.Api(consumer_key=conf.consumer_key,
-                consumer_secret=conf.consumer_secret,
-                access_token_key=conf.oauth_token,
-                access_token_secret=conf.oauth_token_secret)
+        self.api = twitter.Api(conf.consumer_key,
+                conf.consumer_secret,
+                conf.oauth_token,
+                conf.oauth_token_secret)
 
     def updateHomeTimeline (self):
         ''' include_entities return some varius metadata in a structure, not use
