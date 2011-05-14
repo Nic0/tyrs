84f6b78a6e236bb32d89c4318bddebe342e2a840
Nicolas Paris <nicolas.caen@gmail.com>
2011-05-14 20:46:45 +0200

    mistake

--- watchrc.rb
+++
@@ -1,9 +0,0 @@
-# usage/ watchr watchrc.rb
-
-watch '^tyrs/(.*)\.py$' do |match|
-    pychecker match[0]
-end
-
-def pychecker file
-    system("pychecker --stdlib #{file}") if File.exists?(file)
-end
