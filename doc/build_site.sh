f21a231d0f6b42eae0fa3dc1eab77d33091b1b72
Nicolas Paris <nicolas.caen@gmail.com>
2011-05-21 00:25:59 +0200

    screenshot.html added

--- doc/build_site.sh
+++ doc/build_site.sh
@@ -9,6 +9,7 @@ EMAIL="nicolas.caen@gmail.com"
 HTML="asciidoc \
     --conf-file=layout1.conf \
     --attribute stylesdir=css \
+    --attribute iconsdir=images/icons \
     --attribute scriptsdir=javascripts \
     --backend=xhtml11 \
     --attribute icons \
@@ -20,4 +21,5 @@ $HTML --attribute=revision=$VERS --attribute=devel=$DEV \
 --attribute=date=$DATE index.txt
 $HTML --attribute=revision=$VERS quick_start.txt
 $HTML --attribute=revision=$VERS -a toc -a numbered reference.txt
+$HTML --attribute=revision=$VERS screenshot.txt
 #$HTML --attribute=revision=$DEV -a toc -a numbered reference-dev.txt
