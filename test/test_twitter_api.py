3f0f8fbf0906a63c3b2216b6b287fd45ef4cfd4e
Nicolas Paris <nicolas.caen@gmail.com>
2011-06-07 22:34:21 +0200

    test of post_update and update_home_timeline

--- test/test_twitter_api.py
+++ test/test_twitter_api.py
@@ -4,13 +4,10 @@
 import unittest
 import sys
 sys.path.append('../tyrs')
-
-
 from time import gmtime, strftime
-strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
 import tyrs.tyrs as tyrs
 import tyrs.tweets as tweets
+from twitter import TwitterError, Status, User
 
 class TestTwitterApi(unittest.TestCase):
 
@@ -27,13 +24,25 @@ class TestTwitterApi(unittest.TestCase):
         self.api.authentication()
 
     def test_authentication(self):
-        result = self.api.myself.screen_name 
-        self.assertEqual(result, self.username)
+        myself = self.api.myself
+        username = myself.screen_name
+        self.assertIsInstance(myself, User)
+        self.assertEqual(username, self.username)
 
     def test_post_update(self):
         tweet = 'test from unittest at ' + self.get_time()
         result = self.api.post_tweet(tweet, None)
         self.assertEqual(result.text, tweet)
+    
+    def test_post_empty_update(self):
+        tweet = ''
+        with self.assertRaises(TwitterError):
+            self.api.post_tweet(tweet, None)
+
+    def test_update_home_timeline(self):
+        result = self.api.update_home_timeline()
+        self.assertIsInstance(result[0], Status)
+        self.assertIsInstance(result[10], Status)
 
     def get_time(self):
         return strftime('%H:%M:%S', gmtime())
