440b1305e71bd5b0b93e1db472b39b6891469ea2
Nicolas Paris <nicolas.caen@gmail.com>
2011-07-23 19:04:43 +0200

    goo.gl import fixed

--- src/editor.py
+++ src/editor.py
@@ -25,16 +25,15 @@ try:
 except ImportError:
     pass
 
+try:
+    from shorter.googl import GooglUrlShorter
+except ImportError:
+    pass
+
 class Editor(object):
 
     def __init__(self, data=None):
         self.conf = tyrs.container['conf']
-        if self.conf.params['url_shorter'] == 'googl':
-            try:
-                from shorter.googl import GooglUrlShorter
-            except ImportError:
-                pass
-
         self.interface = tyrs.container['interface'] 
         self.interface.refresh_token = True
         self.init_content(data)
